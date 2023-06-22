0b0be43a5d22dc1e90f36f4479eac0a3730bb54d
DESKTOP-GQ982JT\user <aidencjswo@gmail.com>
2023-06-22 13:01:54 +0900

    엑셀시트핸들링

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from openpyxl import Workbook
 
 wb = Workbook()
 
+#select by worksheet title
 ws1 = wb.get_sheet_by_name('Sheet')
 
+#ws1 title rename
 ws1.title = 'aiden'
 
+#두번 째 워크시트 생성
 ws2 = wb.create_sheet('aiden2')
 
 print(wb.sheetnames)
 
-
-
+#save file
 wb.save('balances.xlsx')
